111cb5fa967d00ae88b68b0e4471710aa506a6d9
David Kendall <david.kendall@northumbria.ac.uk>
2017-10-07 17:14:12 +0100

    Toggle WHITE and RED LEDs

--- threadexample.c
+++ threadexample.c
@@ -17,16 +17,30 @@ void *led_toggle_white_thr(void * arg) {
     pthread_exit(NULL);
 }
 
+void *led_toggle_red_thr(void * arg) {
+    while (true) {
+        led_toggle(LED_RED);
+        usleep(500000);
+    }
+    pthread_exit(NULL);
+}
+
 int main (void) {
-    pthread_t thread;
+    pthread_t thread[2];
     int rc;
 
     console_init();
 
-    rc = pthread_create(&thread, NULL, led_toggle_white_thr, NULL);
+    rc = pthread_create(&thread[0], NULL, led_toggle_white_thr, NULL);
     assert(rc == 0);
 
-    rc = pthread_join(thread, NULL);
+    rc = pthread_create(&thread[1], NULL, led_toggle_red_thr, NULL);
+    assert(rc == 0);
+
+    rc = pthread_join(thread[0], NULL);
+    assert(rc == 0);
+
+    rc = pthread_join(thread[1], NULL);
     assert(rc == 0);
 
     exit(0);
