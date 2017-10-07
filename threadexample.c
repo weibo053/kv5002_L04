1584dea09b58aa59a55e749540708d312633fe26
David Kendall <david.kendall@northumbria.ac.uk>
2017-10-07 17:46:09 +0100

    One thread declaration, four instances with different arguments

--- threadexample.c
+++ threadexample.c
@@ -9,34 +9,10 @@
 
 #include "console.h"
 
-void *led_toggle_white_thr(void * arg) {
+void *led_toggle_thr(void * arg) {
     while (true) {
-        led_toggle(LED_WHITE);
+        led_toggle((leds_t)arg);
         usleep(1000000);
-    }
-    pthread_exit(NULL);
-}
-
-void *led_toggle_red_thr(void * arg) {
-    while (true) {
-        led_toggle(LED_RED);
-        usleep(500000);
-    }
-    pthread_exit(NULL);
-}
-
-void *led_toggle_green_thr(void * arg) {
-    while (true) {
-        led_toggle(LED_GREEN);
-        usleep(250000);
-    }
-    pthread_exit(NULL);
-}
-
-void *led_toggle_blue_thr(void * arg) {
-    while (true) {
-        led_toggle(LED_BLUE);
-        usleep(125000);
     }
     pthread_exit(NULL);
 }
@@ -44,32 +20,19 @@ void *led_toggle_blue_thr(void * arg) {
 int main (void) {
     pthread_t thread[4];
     int rc;
+    unsigned long i;
 
     console_init();
 
-    rc = pthread_create(&thread[0], NULL, led_toggle_white_thr, NULL);
-    assert(rc == 0);
+    for (i = 0; i < 4; i += 1) {
+        rc = pthread_create(&thread[i], NULL, led_toggle_thr, (void *)i);
+        assert(rc == 0);
+    }
 
-    rc = pthread_create(&thread[1], NULL, led_toggle_red_thr, NULL);
-    assert(rc == 0);
-
-    rc = pthread_create(&thread[2], NULL, led_toggle_green_thr, NULL);
-    assert(rc == 0);
-
-    rc = pthread_create(&thread[3], NULL, led_toggle_blue_thr, NULL);
-    assert(rc == 0);
-
-    rc = pthread_join(thread[0], NULL);
-    assert(rc == 0);
-
-    rc = pthread_join(thread[1], NULL);
-    assert(rc == 0);
-
-    rc = pthread_join(thread[2], NULL);
-    assert(rc == 0);
-
-    rc = pthread_join(thread[3], NULL);
-    assert(rc == 0);
+    for (i = 0; i < 4; i += 1) {
+        rc = pthread_join(thread[i], NULL);
+        assert(rc == 0);
+    }
 
     exit(0);
 }
