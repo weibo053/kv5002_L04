575ddc810b3c6701c03e15b19a5098f1b713b822
David Kendall <david.kendall@northumbria.ac.uk>
2017-10-07 17:24:10 +0100

    Flash ALL LEDs at various rates

--- threadexample.c
+++ threadexample.c
@@ -25,8 +25,24 @@ void *led_toggle_red_thr(void * arg) {
     pthread_exit(NULL);
 }
 
+void *led_toggle_green_thr(void * arg) {
+    while (true) {
+        led_toggle(LED_GREEN);
+        usleep(250000);
+    }
+    pthread_exit(NULL);
+}
+
+void *led_toggle_blue_thr(void * arg) {
+    while (true) {
+        led_toggle(LED_BLUE);
+        usleep(125000);
+    }
+    pthread_exit(NULL);
+}
+
 int main (void) {
-    pthread_t thread[2];
+    pthread_t thread[4];
     int rc;
 
     console_init();
@@ -37,10 +53,22 @@ int main (void) {
     rc = pthread_create(&thread[1], NULL, led_toggle_red_thr, NULL);
     assert(rc == 0);
 
+    rc = pthread_create(&thread[2], NULL, led_toggle_green_thr, NULL);
+    assert(rc == 0);
+
+    rc = pthread_create(&thread[3], NULL, led_toggle_blue_thr, NULL);
+    assert(rc == 0);
+
     rc = pthread_join(thread[0], NULL);
     assert(rc == 0);
 
     rc = pthread_join(thread[1], NULL);
+    assert(rc == 0);
+
+    rc = pthread_join(thread[2], NULL);
+    assert(rc == 0);
+
+    rc = pthread_join(thread[3], NULL);
     assert(rc == 0);
 
     exit(0);
