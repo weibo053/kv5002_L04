ee4832b628684cb79f4ebd43116d5a41522bba59
David Kendall <david.kendall@northumbria.ac.uk>
2017-10-07 15:59:18 +0100

    Pass argument to thread and retrieve termination status

--- threadexample.c
+++ threadexample.c
@@ -12,20 +12,22 @@ void *simple_thr(void *arg) {
       printf("New thread : pid = %d, tid = 0x%lx, globvar = %d, "
               "var = %s\n", getpid(), (unsigned long)pthread_self(),
               globvar, "Not accessible");
-      pthread_exit(NULL);
+      pthread_exit((void *)(((unsigned long) arg) * 2));
 }
 
 int main(void) {
   int rc;
   int var = 88;
   pthread_t thread;
+  void *retval;
 
   printf("before thread create\n");
-  rc = pthread_create(&thread, NULL, simple_thr, NULL);
+  rc = pthread_create(&thread, NULL, simple_thr, (void *)42);
   assert(rc == 0);
-  pthread_join(thread, NULL);
+  pthread_join(thread, &retval);
   printf("Main thread: pid = %d, tid = 0x%lx, globvar = %d, " 
          "var = %d\n", getpid(), (unsigned long)pthread_self(), globvar, var);
+  printf("Thread termination status = %ld\n", (unsigned long)retval);
   exit(0);
 }
 
