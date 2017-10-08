2f73169b92b8ed47acd38a603bb043c290b3ffd0
David Kendall <david.kendall@northumbria.ac.uk>
2017-10-08 13:39:53 +0100

    Flash all LEDS at various rates based on one thread declaration

--- threadexample.c
+++ threadexample.c
@@ -9,23 +9,37 @@
 
 #include "console.h"
 
+typedef struct {
+    leds_t led;
+    int    rate;
+} thread_args_t;
+
 void *led_toggle_thr(void * arg) {
+    leds_t led = ((thread_args_t *)arg)->led;
+    int delay = (int)(1000000 / ((thread_args_t *)arg)->rate);
+
     while (true) {
-        led_toggle((leds_t)arg);
-        usleep(1000000);
+        led_toggle(led);
+        usleep(delay);
     }
     pthread_exit(NULL);
 }
 
 int main (void) {
     pthread_t thread[4];
+    thread_args_t args[] = {
+        {LED_WHITE, 1},
+        {LED_RED,   2},
+        {LED_GREEN, 4},
+        {LED_BLUE,  8}
+    };
     int rc;
     unsigned long i;
 
     console_init();
 
     for (i = 0; i < 4; i += 1) {
-        rc = pthread_create(&thread[i], NULL, led_toggle_thr, (void *)i);
+        rc = pthread_create(&thread[i], NULL, led_toggle_thr, &args[i]);
         assert(rc == 0);
     }
 
